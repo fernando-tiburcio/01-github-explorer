990d14da8796aa5f7732e468f6a18a4dfc358e8d
Fernando Tiburcio <fernando.tiburcio@agriness.com>
2022-04-16 11:50:33 -0300

    first commit

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,17 @@
+interface IRepositoryProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+}
+
+export function RepositoryItem({ repository }: IRepositoryProps): JSX.Element {
+  return (
+    <li>
+      <strong>{repository.name}</strong>
+      <p>{repository.description}</p>
+      <a href={repository.html_url}>Acessar repositório</a>
+    </li>
+  );
+}
